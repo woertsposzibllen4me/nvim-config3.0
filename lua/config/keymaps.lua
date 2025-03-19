cb278b87573a34c76e8b7767447f6ae42475c66a
Loïc Villet <villet.loic@gmail.com>
2025-03-19 13:54:29 +0100

    Add lsp rename basic keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -59,6 +59,7 @@ map("n", "<esc>", "<cmd>noh<cr>")
 
 -- lsp stuff
 map("n", "K", vim.lsp.buf.hover, {})
+map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol under cursor" })
 map("n", "gd", require("telescope.builtin").lsp_definitions, { desc = "Telescope Goto Definition" })
 map("n", "gr", require("telescope.builtin").lsp_references, { desc = "Telescope Goto References" })
 
