ba4557037ebfd211b4e8223207971e9d3e20b2eb
Loïc Villet <villet.loic@gmail.com>
2025-03-23 15:40:13 +0100

    Add neoscroll

---
+++ lua/plugins/neoscroll.lua
@@ -0,0 +1,6 @@
+return {
+  "karb94/neoscroll.nvim",
+  enabled = true,
+  event = "bufenter",
+  opts = { easing = "linear", stop_eof = true, mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>" } },
+}
