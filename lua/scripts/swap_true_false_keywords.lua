4c0a830be8e3985cdad011f2065bcd362e768d15
Loïc Villet <villet.loic@gmail.com>
2025-03-24 13:48:19 +0100

    Make true false swapper work on whole line

--- lua/scripts/swap_true_false_keywords.lua
+++ lua/scripts/swap_true_false_keywords.lua
@@ -1,9 +1,6 @@
--- Swap true/false under cursor
 vim.keymap.set("n", "<leader>S", function()
-  -- Get current word under cursor
-  local word = vim.fn.expand("<cword>")
+  local line = vim.api.nvim_get_current_line()
 
-  -- Define replacements
   local replacements = {
     ["true"] = "false",
     ["false"] = "true",
@@ -11,20 +8,20 @@ vim.keymap.set("n", "<leader>S", function()
     ["False"] = "True",
   }
 
-  if replacements[word] then
-    -- Save cursor position
-    local cursor_pos = vim.api.nvim_win_get_cursor(0)
-
-    -- Enter visual mode (v) and select inner word (iw) then change
-    vim.cmd('normal! viw"_c' .. replacements[word])
-
-    -- Return to normal mode and restore cursor position
-    vim.api.nvim_win_set_cursor(0, cursor_pos)
-
-    -- save the file
-    vim.cmd("write")
+  -- Check for any match in the line
+  for pattern, replacement in pairs(replacements) do
+    if line:find(pattern) then
+      -- Replace the first occurrence
+      local new_line = line:gsub(pattern, replacement, 1)
+      vim.api.nvim_set_current_line(new_line)
+      vim.cmd("write")
+      return true
+    end
   end
+
+  return false
 end, { desc = "Swap true/false keywords" })
+
 require("which-key").add({
   { "<leader>S", group = "Swap true/false keywords", icon = "" },
 })
