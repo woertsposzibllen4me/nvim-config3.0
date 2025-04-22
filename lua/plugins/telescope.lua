f68baefbdd0464fb73192cddba136b06eafa80d2
Loïc Villet <villet.loic@gmail.com>
2025-04-23 00:23:19 +0200

    Add build steps for fzf native extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,12 @@
 return {
   "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+    },
+  },
   enabled = true,
   event = "VeryLazy",
   _keys = { -- Disabled for now in favor of snack pickers
