907832055471a3c4430430562c926eef75b9f4de
Loïc Villet <villet.loic@gmail.com>
2025-05-04 22:44:11 +0200

    Add flip-flop comments scripts
    relies on comment  nvim plugin

--- lua/plugins/vim-capslock.lua
+++ lua/plugins/vim-capslock.lua
@@ -2,6 +2,5 @@ return {
   "tpope/vim-capslock",
   keys = {
     { "<C-g>c", mode = "i" }, -- for insert mode
-    { "gC", mode = { "n" }, desc = "Toggle Capslock" }, -- optional: include normal/visual mode mappings
   },
 }
