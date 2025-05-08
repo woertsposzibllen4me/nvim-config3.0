13276f805776b0db08b3f47e116a94c80b1818a2
Loïc Villet <villet.loic@gmail.com>
2025-05-08 09:05:08 +0200

    Testing enabling snacks input

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -10,7 +10,7 @@ return {
     dashboard = require("modules.snacks.dashboard-config"),
     -- explorer = { enabled = true },
     -- indent = { enabled = true },
-    -- input = { enabled = true },
+    input = { enabled = true },
     notifier = { enabled = true },
     -- quickfile = { enabled = true },
     -- scope = { enabled = true },
