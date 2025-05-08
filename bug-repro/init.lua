bfffabf3f3884f8a11d6583404fd93552d54e1f4
Loïc Villet <villet.loic@gmail.com>
2025-05-08 20:53:33 +0200

    Add bug repro config to repo

---
+++ bug-repro/init.lua
@@ -0,0 +1,39 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Basic Neovim settings
+vim.g.mapleader = " " -- Set leader key to space
+vim.opt.termguicolors = true -- Enable true color support
+
+-- Plugin specifications
+require("lazy").setup({
+  -- Snacks plugin
+  {
+    "folke/snacks.nvim",
+    --stylua: ignore
+    dashboard = {enabled = true,
+    sections = {
+    { section = "header" },
+    { section = "keys"},
+    {
+      pane = 1,
+      section = "terminal",
+      cmd = "echo 'test'",
+      height = 5,
+      padding = 1,
+    },
+
+    },
+  },
+  },
+})
