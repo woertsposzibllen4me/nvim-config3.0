b80594fa89d4b222061e184040114aff11bba240
Loïc Villet <villet.loic@gmail.com>
2025-05-10 22:59:01 +0200

    Disable avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,6 +2,7 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
+  enabled = false,
   opts = {
     provider = "claude",
     claude = {
