2b4e0a64da85196354d473147d38d3bb8c23600e
Loïc Villet <villet.loic@gmail.com>
2025-05-17 23:28:17 +0200

    Add hlslens plugin

---
+++ lua/plugins/hlslens.lua
@@ -0,0 +1,21 @@
+local kopts = { noremap = true, silent = true }
+return {
+  "kevinhwang91/nvim-hlslens",
+  opts = {},
+  keys = {
+    {
+      "n",
+      [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+      kopts,
+    },
+    {
+      "N",
+      [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+      kopts,
+    },
+    { "*", [[*<Cmd>lua require('hlslens').start()<CR>]], kopts },
+    { "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts },
+    { "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts },
+    { "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts },
+  },
+}
