ec25a1eac86b91c8878806a3341794a48c6b1a34
Loïc Villet <villet.loic@gmail.com>
2025-05-19 05:56:10 +0200

    Fix powershell_es lsp issue in diffview (type shi)

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -78,6 +78,19 @@ return {
       end,
     })
 
+    -- Prevent powershell_es mess
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        local name = vim.api.nvim_buf_get_name(bufnr)
+        if client and client.name == "powershell_es" and name:match("^diffview://") then
+          ---@diagnostic disable-next-line: param-type-mismatch -- go fuck yourself
+          client.stop(client.name)
+        end
+      end,
+    })
+
     require("diffview").setup({
       hooks = {
         view_opened = function(view)
