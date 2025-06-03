16c999db37ba41a034cd924c3a32a434088a0b4c
Loïc Villet <villet.loic@gmail.com>
2025-06-03 20:59:52 +0200

    Add mason-null-ls plugin

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,32 +1,39 @@
 return {
-  {
+  "jay-babu/mason-null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "williamboman/mason.nvim",
     "nvimtools/none-ls.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local null_ls = require("null-ls")
-      local h = require("null-ls.helpers")
-      local methods = require("null-ls.methods")
-
-      -- taplo formatter for toml files
-      local taplo = h.make_builtin({
-        name = "taplo",
-        method = methods.internal.FORMATTING,
-        filetypes = { "toml" },
-        generator_opts = {
-          command = "taplo",
-          args = { "format", "-" },
-          to_stdin = true,
-        },
-        factory = h.formatter_factory,
-      })
-
-      null_ls.setup({
-        sources = {
-          taplo,
-        },
-        autostart = true,
-      })
-    end,
   },
+  config = function()
+    local h = require("null-ls.helpers")
+    local methods = require("null-ls.methods")
+
+    require("mason-null-ls").setup({
+      ensure_installed = {
+        "taplo",
+        "pylint",
+      },
+      automatic_installation = true,
+    })
+
+    local taplo = h.make_builtin({
+      name = "taplo",
+      method = methods.internal.FORMATTING,
+      filetypes = { "toml" },
+      generator_opts = {
+        command = "taplo",
+        args = { "format", "-" },
+        to_stdin = true,
+      },
+      factory = h.formatter_factory,
+    })
+
+    require("null-ls").setup({
+      sources = {
+        taplo,
+      },
+      autostart = true,
+    })
+  end,
 }
