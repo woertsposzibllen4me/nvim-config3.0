d0563ee824bcf0fb5dc53e1d6a60ae64dc1407f7
Loïc Villet <villet.loic@gmail.com>
2025-06-05 01:05:21 +0200

    Add actions preview plugin

--- lua/modules/snacks/custom_layouts.lua
+++ lua/modules/snacks/custom_layouts.lua
@@ -32,4 +32,19 @@ return {
       { win = "preview", title = "{preview}", border = "rounded" },
     },
   },
+  midscreen_dropdown_preview = {
+    -- preview = true,
+    layout = {
+      backdrop = false,
+      row = 0.1,
+      width = 0.55,
+      min_width = 80,
+      height = 0.75,
+      border = "none",
+      box = "vertical",
+      { win = "preview", title = "{preview}", height = 0.6, border = "rounded" },
+      { win = "input", height = 1, border = "rounded", title = "{title} {live} {flags}", title_pos = "center" },
+      { win = "list", border = "hpad" },
+    },
+  },
 }
---
+++ lua/plugins/actions-preview.lua
@@ -0,0 +1,24 @@
+return {
+  "aznhe21/actions-preview.nvim",
+  opts = {
+    backend = {
+      "snacks",
+      -- "telescope",
+      -- "minipick",
+      -- "nui",
+    },
+    snacks = {
+      layout = require("modules.snacks.custom_layouts").midscreen_dropdown_preview,
+    },
+  },
+  keys = {
+    {
+      "<leader>cp",
+      function()
+        require("actions-preview").code_actions()
+      end,
+      desc = "Preview Code Actions",
+      mode = { "n", "v" },
+    },
+  },
+}
