9874dc7102e7ed1c181b2bc171b39239f789078d
Loïc Villet <villet.loic@gmail.com>
2025-06-05 01:05:42 +0200

    Tweak fastaction ruff code actions order (doesnt work xd)

--- lua/plugins/fastaction.lua
+++ lua/plugins/fastaction.lua
@@ -1,13 +1,21 @@
 return {
   "Chaitanyabsprip/fastaction.nvim",
-  opts = {},
+  enabled = true,
+  opts = {
+    priority = {
+      ruff = {
+        { pattern = "organize import", key = "o", order = 98 },
+        { pattern = "fix all", key = "A", order = 99 },
+      },
+    },
+  },
   keys = {
     {
-      "<leader>ca",
+      "<leader>cx",
       function()
         require("fastaction").code_action()
       end,
-      desc = "Display code actions",
+      desc = "Fast Code Action",
       mode = { "n", "x" },
     },
   },
