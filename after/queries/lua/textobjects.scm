226a45f5bb26adfbf557bdb372e9a7c11aa3793d
Loïc Villet <villet.loic@gmail.com>
2025-06-08 03:15:35 +0200

    Enhance treesitter textobjects motions (add query files)
    Add some new textobjects motions for lua and python

---
+++ after/queries/lua/textobjects.scm
@@ -0,0 +1,23 @@
+; extends
+
+; Capture function names in function calls
+(function_call
+  name: (identifier) @function_name)
+
+; Capture method names in method calls (dot notation)
+(function_call
+  name: (dot_index_expression
+    field: (identifier) @method_name))
+
+; ADD THIS: Capture method names in method calls (colon notation)
+(function_call
+  name: (method_index_expression
+    method: (identifier) @method_name))
+
+; Capture all call names (function, dot method, colon method)
+(function_call
+  name: [
+    (identifier) @call_name
+    (dot_index_expression field: (identifier) @call_name)
+    (method_index_expression method: (identifier) @call_name)
+  ])
---
+++ after/queries/python/textobjects.scm
@@ -0,0 +1,26 @@
+; extends
+
+; Capture function names in function calls
+(call
+  function: (identifier) @function_name)
+
+; Capture method names in method calls
+(call
+  function: (attribute
+    attribute: (identifier) @method_name))
+
+; Capture both function name or method name
+(call
+  function: [
+    (identifier) @call_name
+    (attribute attribute: (identifier) @call_name)
+  ])
+
+; Capture return type annotations
+(function_definition
+  return_type: (_) @return_type)
+
+; Capture function parameters
+(function_definition
+  parameters: (parameters) @function_parameters
+  !return_type)
