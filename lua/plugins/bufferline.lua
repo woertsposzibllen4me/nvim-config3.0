120045c4c141e55f60de6c2f3eeb77b0502a72f2
Loïc Villet <villet.loic@gmail.com>
2025-06-09 02:39:06 +0200

    tweak bufferline appearance

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,21 +4,26 @@ return {
   dependencies = "nvim-tree/nvim-web-devicons",
   event = { "BufReadPost", "BufNewFile" },
   enabled = true,
-  config = function()
-    require("bufferline").setup({
-      options = {
-        mode = "tabs",
-        max_name_length = 30,
-        name_formatter = function(buf)
-          ---@cast buf {name: string, path: string, bufnr: number, buffers: table, tabnr: number}
-          -- Check if tab has custom name
-          if vim.t[buf.tabnr] and vim.t[buf.tabnr].custom_tabname then
-            return vim.t[buf.tabnr].custom_tabname
-          end
-          -- Otherwise use default name
-          return buf.name
-        end,
+  opts = {
+    options = {
+      mode = "tabs",
+      max_name_length = 30,
+      always_show_bufferline = false,
+      name_formatter = function(buf)
+        ---@cast buf {name: string, path: string, bufnr: number, buffers: table, tabnr: number}
+        -- Check if tab has custom name
+        if vim.t[buf.tabnr] and vim.t[buf.tabnr].custom_tabname then
+          return vim.t[buf.tabnr].custom_tabname
+        end
+        -- Otherwise use default name
+        return buf.name
+      end,
+    },
+    highlights = {
+      fill = {
+        -- bg = "#1d1f2e",
+        link = "StatusLine",
       },
-    })
-  end,
+    },
+  },
 }
