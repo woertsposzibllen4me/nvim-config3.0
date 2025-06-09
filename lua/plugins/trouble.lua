a741e1138fcc658301a27839b2225df6d7071bc9
Loïc Villet <villet.loic@gmail.com>
2025-06-09 21:56:53 +0200

    Adjust trouble config to use ]t [t for specific nav + few tweaks
    Adjust trouble config to use ]t [t for specific nav + few tweaks

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -8,41 +8,34 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require("trouble").setup(opts)
+    -- Navigate trouble only
+    vim.keymap.set("n", "[t", function()
+      if require("trouble").is_open() then
+        ---@ diagnostic disable-next-line: missing-fields, missing-parameter
+        require("trouble").prev({ skip_groups = true, jump = true })
+      end
+    end, { desc = "Previous Trouble Item" })
+
+    vim.keymap.set("n", "]t", function()
+      if require("trouble").is_open() then
+        ---@ diagnostic disable-next-line: missing-fields, missing-parameter
+        require("trouble").next({ skip_groups = true, jump = true })
+      end
+    end, { desc = "Next Trouble Item" })
+
+    vim.keymap.set("n", "<leader>xf", function()
+      if require("trouble").is_open() then
+        ---@ diagnostic disable-next-line: missing-parameter
+        require("trouble").focus()
+      end
+    end, { desc = "Focus Trouble Window" })
+  end,
   keys = {
-    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
-    { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+    { "<leader>xo", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+    { "<leader>xi", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
     { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
-    { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
-    {
-      "[q",
-      function()
-        if require("trouble").is_open() then
-          require("trouble").prev({ skip_groups = true, jump = true })
-        else
-          local ok, err = pcall(vim.cmd.cprev)
-          if not ok then
-            vim.notify(err, vim.log.levels.ERROR)
-          end
-        end
-      end,
-      desc = "Previous Trouble/Quickfix Item",
-    },
-    {
-      "]q",
-      function()
-        if require("trouble").is_open() then
-          require("trouble").next({ skip_groups = true, jump = true })
-        else
-          local ok, err = pcall(vim.cmd.cnext)
-          if not ok then
-            vim.notify(err, vim.log.levels.ERROR)
-          end
-        end
-      end,
-      desc = "Next Trouble/Quickfix Item",
-    },
+    { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
   },
-  -- config = function()
-  --   require("which-key").add({ "<leader>x", group = "Diagnostics" })
-  -- end,
 }
