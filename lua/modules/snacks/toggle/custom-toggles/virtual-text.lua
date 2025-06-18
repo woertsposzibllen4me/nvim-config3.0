310ae95f1d9cbf2579d0f83dcee9eaebfa8ed5fd
Loïc Villet <villet.loic@gmail.com>
2025-06-18 18:51:38 +0200

    minor fix fluff

--- lua/modules/snacks/toggle/custom-toggles/virtual-text.lua
+++ lua/modules/snacks/toggle/custom-toggles/virtual-text.lua
@@ -3,7 +3,11 @@ M.virtual_text_toggle = Snacks.toggle.new({
   name = "Diagnostic Virtual Text",
   get = function()
     local config = vim.diagnostic.config()
-    return config.virtual_text ~= false
+    if config then
+      return config.virtual_text ~= false
+    else
+      return false -- Default to false if config is not available
+    end
   end,
   set = function(state)
     vim.diagnostic.config({ virtual_text = state })
