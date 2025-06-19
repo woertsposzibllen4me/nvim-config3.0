133fb84355dfd39b480bbe7a3684aa3515a507cf
Loïc Villet <villet.loic@gmail.com>
2025-06-20 01:00:52 +0200

    Add custom return capabilities to Snacks explorer to all custom pickers launched from it

--- lua/modules/snacks/explorer/explorer-config.lua
+++ lua/modules/snacks/explorer/explorer-config.lua
@@ -1,9 +1,5 @@
 local M = {}
 
--- Used to fix a visual bug that happens if we run another snack picker while having the Snacks
--- Explorer in focus right before their launch.
-local focus_large_win = require("scripts.ui.focus-largest-window")
-
 local create_return_action = function(current_win, cursor_pos)
   return function(picker)
     picker:close()
@@ -12,36 +8,57 @@ local create_return_action = function(current_win, cursor_pos)
   end
 end
 
+-- Wrapper function to launch any picker with return-to-explorer capability
+local launch_picker_with_return = function(picker_fn, config)
+  local current_win = vim.api.nvim_get_current_win()
+  local cursor_pos = vim.api.nvim_win_get_cursor(current_win)
+
+  vim.schedule(function()
+    -- Fix a visual bug that happens if we run another Snacks picker while having the Snacks
+    -- explorer in focus right before their launch.
+    vim.cmd("wincmd l")
+
+    -- Merge the return action into the config
+    config.actions = config.actions or {}
+    config.actions.return_to_explorer = create_return_action(current_win, cursor_pos)
+
+    -- Add "return with explorer in focus" escape key mapping
+    config.win = config.win or {}
+    config.win.input = config.win.input or {}
+    config.win.input.keys = config.win.input.keys or {}
+    if not config.win.input.keys["<esc>"] then
+      config.win.input.keys["<esc>"] = "return_to_explorer"
+    end
+
+    picker_fn(config)
+  end)
+end
+
 M.grep_for_filename = function(picker, item)
   if not item or not item.file then
     return
   end
-  -- Extract just the filename without path and extension
+
   local filename = vim.fn.fnamemodify(item.file, ":t:r")
-  vim.schedule(function()
-    focus_large_win.focus()
-    Snacks.picker.grep_word({
-      title = "Grep for: " .. filename,
-      search = filename,
-      cwd = picker:cwd(), -- Use the same cwd as the explorer
-    })
-  end)
+
+  launch_picker_with_return(Snacks.picker.grep_word, {
+    title = "Grep for: " .. filename,
+    search = filename,
+    cwd = picker:cwd(),
+  })
 end
 
-M.grep_for_full_filename = function(picker, item)
+M.grep_for_filename_with_ext = function(picker, item)
   if not item or not item.file then
     return
   end
-  -- Extract filename with extension
   local filename = vim.fn.fnamemodify(item.file, ":t")
-  vim.schedule(function()
-    focus_large_win.focus()
-    Snacks.picker.grep_word({
-      title = "Grep for: " .. filename,
-      search = filename,
-      cwd = picker:cwd(),
-    })
-  end)
+
+  launch_picker_with_return(Snacks.picker.grep_word, {
+    title = "Grep for: " .. filename,
+    search = filename,
+    cwd = picker:cwd(),
+  })
 end
 
 M.grep_in_dir = function(picker, item)
@@ -59,65 +76,40 @@ M.grep_in_dir = function(picker, item)
   local title = "Grep in: " .. vim.fn.fnamemodify(path, ":~:.")
   local dirs = { path }
 
-  vim.schedule(function()
-    focus_large_win.focus()
-
-    local grep_config = {
-      title = title,
-      dirs = dirs,
-      win = {
-        input = {
-          -- We explicitly pass dirs and title to the key setup
-          keys = require("modules.snacks.picker.keys.setup-all-keys").setup_grep_input_keys(dirs, title),
-        },
+  launch_picker_with_return(Snacks.picker.grep, {
+    title = title,
+    dirs = dirs,
+    win = {
+      input = {
+        keys = require("modules.snacks.picker.keys.setup-all-keys").setup_grep_input_keys(dirs, title),
       },
-    }
-
-    Snacks.picker.grep(grep_config)
-  end)
+    },
+  })
 end
 
 M.search_files_in_dir = function(picker, item)
   if not item or not item.file then
     return
   end
-
-  local current_win = vim.api.nvim_get_current_win()
-  local cursor_pos = vim.api.nvim_win_get_cursor(current_win)
-
   local path
   if vim.fn.isdirectory(item.file) == 1 then
     path = item.file
   else
     path = vim.fn.fnamemodify(item.file, ":h")
   end
-
   local title = "Search files in: " .. vim.fn.fnamemodify(path, ":~:.")
   local dirs = { path }
 
-  vim.schedule(function()
-    focus_large_win.focus()
-    Snacks.picker.files({
-      title = title,
-      dirs = dirs,
-      actions = {
-        return_to_explorer = create_return_action(current_win, cursor_pos),
-      },
-      win = {
-        input = {
-          keys = {
-            ["<esc>"] = "return_to_explorer",
-          },
-        },
-      },
-    })
-  end)
+  launch_picker_with_return(Snacks.picker.files, {
+    title = title,
+    dirs = dirs,
+  })
 end
 
 return {
   actions = {
     grep_filename = M.grep_for_filename,
-    grep_full_filename = M.grep_for_full_filename,
+    grep_full_filename = M.grep_for_filename_with_ext,
     grep_in_dir = M.grep_in_dir,
     search_files_in_dir = M.search_files_in_dir,
   },
@@ -134,8 +126,7 @@ return {
           function()
             vim.cmd("wincmd l")
           end,
-          desc = "Swap to right window", -- it looks like, because explorer is a float, going to the
-          -- right window actually just goes to the last window used, which is pretty handy
+          desc = "Exit to right window",
         },
       },
     },
@@ -145,7 +136,7 @@ return {
           function()
             vim.cmd("wincmd l")
           end,
-          desc = "Swap to right window",
+          desc = "Exit to right window",
         },
       },
     },
