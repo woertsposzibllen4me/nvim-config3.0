51eb88c90c51678fcb7c070c8130f97bfd51026f
Loïc Villet <villet.loic@gmail.com>
2025-06-20 02:08:38 +0200

    Disable bqf auto-preview

--- lua/plugins/bqf.lua
+++ lua/plugins/bqf.lua
@@ -3,6 +3,7 @@ return {
   ft = "qf",
   opts = {
     preview = {
+      auto_preview = false,
       winblend = 0,
       should_preview_cb = function(_, qwinid)
         local title = vim.fn.getqflist({ winid = qwinid, title = 0 }).title
