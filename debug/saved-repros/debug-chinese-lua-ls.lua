5f688de4a1f7c90543472c0d4302914d0a5b5f5f
Loïc Villet <villet.loic@gmail.com>
2025-06-23 14:53:04 +0200

    Fix duplicate code in lua-ls saved repro

--- debug/saved-repros/debug-chinese-lua-ls.lua
+++ debug/saved-repros/debug-chinese-lua-ls.lua
@@ -18,16 +18,6 @@ require("lazy.minit").repro({
         },
       },
     },
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    enabled = false,
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    },
     {
       "folke/noice.nvim",
       opts = {
