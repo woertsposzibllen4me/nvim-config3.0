96364262ab460c534718273ba53c5cb56a3f7839
Loïc Villet <villet.loic@gmail.com>
2025-06-24 20:53:36 +0200

    Make copilot virtual text a tiny bit more visible
    And document some stuff about HL groups

--- lua/config/highlight-groups.lua
+++ lua/config/highlight-groups.lua
@@ -1,18 +1,19 @@
--- vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#631010" })
+-- Less intrusive folding
 vim.api.nvim_set_hl(0, "Folded", {
   -- bg = "#27283d",
   fg = "#5a5b5c",
 })
 
+-- Word diff
 vim.api.nvim_set_hl(0, "DiffText", { bg = "#633f01" })
--- #633f01 pretty good, lean on brighter rather than darker if ur gonna adjust
--- #6e4601 trying out
 
--- vim.api.nvim_set_hl(0, "DiffChange", { bg = "#252A3F" }) -- Original
-
--- vim.api.nvim_set_hl(0, "DiffText", { bg = "#252A3F" })
--- vim.api.nvim_set_hl(0, "DiffChange", { bg = "#222436" })
-
+-- Clearer match in pickers
 vim.api.nvim_set_hl(0, "CustomMatch", { fg = "#73ffdc", bold = true })
 vim.api.nvim_set_hl(0, "CustomMatchFuzzy", { fg = "#f1ff73", bold = true })
+
+-- Darker terminal
 vim.api.nvim_set_hl(0, "CustomTerminalBg", { bg = "#1d243b" })
+
+-- Sligthly more visible Copilot hints
+vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#495169" })
+vim.api.nvim_set_hl(0, "CopilotAnnotation", { fg = "#495169" })
