45c76d9e609677bb07a9f5f5b8bef26da73b0f2d
Loïc Villet <villet.loic@gmail.com>
2026-01-06 11:51:14 +0100

    Change toggle diag hotkey to xt over xd

--- lua/modules/snacks/toggle/toggle-setup.lua
+++ lua/modules/snacks/toggle/toggle-setup.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd("User", {
     Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
     Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
     Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-    Snacks.toggle.diagnostics():map("<leader>xd")
+    Snacks.toggle.diagnostics():map("<leader>xt")
     Snacks.toggle
       .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
       :map("<leader>uc")
