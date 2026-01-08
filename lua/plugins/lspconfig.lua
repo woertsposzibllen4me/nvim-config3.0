a76ab3043e99d2cc2bb19d65a19f2c1cdc14fc05
Loïc Villet <villet.loic@gmail.com>
2026-01-08 14:04:10 +0100

    Remove Ruff lsp fake edit trick (not needed anymore)

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -166,16 +166,6 @@ return {
       -- Ruff for formatting and diagnostics
       vim.lsp.config("ruff", {
         capabilities = capabilities,
-        on_attach = function(_, bufnr)
-          -- Make a fake edit to trigger diagnostics directly after attaching
-          -- (otherwise wouldn't until a change is made to the buffer)
-          vim.schedule(function()
-            if vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].modifiable then
-              vim.api.nvim_buf_set_text(bufnr, 0, 0, 0, 0, { "" })
-              vim.bo[bufnr].modified = false
-            end
-          end)
-        end,
         init_options = {
           settings = {
             -- exclude = python_lib_paths,
