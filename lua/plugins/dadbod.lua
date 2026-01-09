9b7d55865ed2547ef1633d2d2f7ed879a706010b
Loïc Villet <villet.loic@gmail.com>
2026-01-09 16:05:09 +0100

    wip

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -14,17 +14,46 @@ return {
       "DBUIFindBuffer",
     },
     keys = {
-      { "<leader>Du", ":DBUIToggle<CR>", desc = "Toggle database UI" },
-      { "<leader>Df", ":DBUIFindBuffer<CR>", desc = "Find database buffer" },
-      { "<leader>Dr", ":DBUIRenameBuffer<CR>", desc = "Rename database buffer" },
-      { "<leader>Dl", ":DBUILastQueryInfo<CR>", desc = "Show last query info" },
+      {
+        "<leader>DD",
+        function()
+          local dbui_tab = nil
+
+          -- Search through all tabs for DBUI buffer
+          for tab = 1, vim.fn.tabpagenr("$") do
+            local buflist = vim.fn.tabpagebuflist(tab)
+            for _, buf in ipairs(buflist) do
+              if vim.fn.getbufvar(buf, "&filetype") == "dbui" then
+                dbui_tab = tab
+                break
+              end
+            end
+            if dbui_tab then
+              break
+            end
+          end
+
+          if dbui_tab then
+            vim.cmd("tabnext " .. dbui_tab)
+          else
+            vim.cmd("tabnew")
+            vim.cmd("DBUI")
+          end
+        end,
+        desc = "Toggle database UI in tab",
+      },
+      { "<leader>Df", "<cmd>DBUIFindBuffer<CR>", desc = "Find database buffer" },
+      { "<leader>Dr", "<cmd>DBUIRenameBuffer<CR>", desc = "Rename database buffer" },
+      { "<leader>Dl", "<cmd>DBUILastQueryInfo<CR>", desc = "Show last query info" },
     },
     config = function(_, opts)
       vim.g.db_ui_save_location = vim.fn.stdpath("config") .. "/db_ui"
+      vim.g.db_ui_use_nerd_fonts = 1
+
       vim.api.nvim_create_autocmd("FileType", {
         pattern = { "sql", "mysql", "plsql" },
         callback = function()
-          vim.schedule(opts.db_completion)
+          require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
         end,
       })
     end,
