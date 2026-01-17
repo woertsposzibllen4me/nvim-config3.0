fb8df3c114d8c15c98b73213fc631076e7b76432
Loïc Villet <villet.loic@gmail.com>
2026-01-17 21:13:18 +0100

    make python docstrings look like comments (grey) in tokyonight

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,5 +2,12 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
+  opts = {
+    on_highlights = function(hl, c)
+      hl["@string.documentation.python"] = {
+        fg = c.comment,
+        italic = false,
+      }
+    end,
+  },
 }
