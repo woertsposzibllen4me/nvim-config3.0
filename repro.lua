a15f8e88c2019799035a1b8946003389b9496651
Loïc Villet <villet.loic@gmail.com>
2026-01-17 13:25:08 +0100

    Update repro template

--- repro.lua
+++ repro.lua
@@ -1,7 +1,8 @@
 -- usage "nvim -u this_filename.lua"
-vim.env.LAZY_STDPATH = ".repro"
+vim.env.LAZY_STDPATH = vim.fn.stdpath("data") .. "/.repro"
 load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
-vim.g.mapleader = " " -- Set leader key to space
+vim.g.mapleader = " "
+vim.opt.swapfile = false
 
 --- @ diagnostic disable: missing-fields
 require("lazy.minit").repro({
